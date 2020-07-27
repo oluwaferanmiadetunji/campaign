26a1b8299f2207a63dbe9b3e59894886066fc51c
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-07-27 18:23:21 +0100

    Added routing

--- .next/dist/components/Layout.js
+++ .next/dist/components/Layout.js
@@ -14,6 +14,10 @@ var _Header2 = _interopRequireDefault(_Header);
 
 var _semanticUiReact = require('semantic-ui-react');
 
+var _head = require('next\\dist\\lib\\head.js');
+
+var _head2 = _interopRequireDefault(_head);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = 'C:\\Users\\Adetunji Oluwaferanm\\Documents\\Adetunji Oluwaferanmi\\Crypto\\campaign\\components\\Layout.js';
@@ -22,13 +26,22 @@ exports.default = function (props) {
 	return _react2.default.createElement(_semanticUiReact.Container, {
 		__source: {
 			fileName: _jsxFileName,
-			lineNumber: 7
+			lineNumber: 8
 		}
-	}, _react2.default.createElement(_Header2.default, {
+	}, _react2.default.createElement(_head2.default, {
 		__source: {
 			fileName: _jsxFileName,
-			lineNumber: 8
+			lineNumber: 9
+		}
+	}, _react2.default.createElement('link', { rel: 'stylesheet', href: '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css', __source: {
+			fileName: _jsxFileName,
+			lineNumber: 10
+		}
+	})), _react2.default.createElement(_Header2.default, {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 12
 		}
 	}), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXExheW91dC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkhlYWRlciIsIkNvbnRhaW5lciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFPLEFBQVk7Ozs7QUFDbkIsQUFBUyxBQUVUOzs7Ozs7a0JBQWUsVUFBQSxBQUFDLE9BQVUsQUFDekI7d0JBQ0MsQUFBQzs7YUFBRDtlQUFBLEFBQ0M7QUFERDtBQUFBLEVBQUEsa0JBQ0MsQUFBQzs7YUFBRDtlQURELEFBQ0MsQUFDQztBQUREO0FBQUEsV0FGRixBQUNDLEFBRVEsQUFHVDtBQVBEIiwiZmlsZSI6IkxheW91dC5qcyIsInNvdXJjZVJvb3QiOiJDOi9Vc2Vycy9BZGV0dW5qaSBPbHV3YWZlcmFubS9Eb2N1bWVudHMvQWRldHVuamkgT2x1d2FmZXJhbm1pL0NyeXB0by9jYW1wYWlnbiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXExheW91dC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkhlYWRlciIsIkNvbnRhaW5lciIsIkhlYWQiLCJwcm9wcyIsImNoaWxkcmVuIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7O0FBQ1AsQUFBTyxBQUFZOzs7O0FBQ25CLEFBQVM7O0FBQ1QsQUFBTyxBQUVQOzs7Ozs7OztrQkFBZSxVQUFBLEFBQUMsT0FBVSxBQUN6Qjt3QkFDQyxBQUFDOzthQUFEO2VBQUEsQUFDQztBQUREO0FBQUEsRUFBQSxrQkFDQyxBQUFDOzthQUFEO2VBQUEsQUFDQztBQUREO0FBQUEsNENBQ08sS0FBTixBQUFVLGNBQWEsTUFBdkIsQUFBNEI7YUFBNUI7ZUFGRixBQUNDLEFBQ0MsQUFFRDtBQUZDO3NCQUVELEFBQUM7O2FBQUQ7ZUFKRCxBQUlDLEFBQ0M7QUFERDtBQUFBLFdBTEYsQUFDQyxBQUtRLEFBR1Q7QUFWRCIsImZpbGUiOiJMYXlvdXQuanMiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvQWRldHVuamkgT2x1d2FmZXJhbm0vRG9jdW1lbnRzL0FkZXR1bmppIE9sdXdhZmVyYW5taS9DcnlwdG8vY2FtcGFpZ24ifQ==
--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,6 @@ class CampaignIndex extends React.Component {
 		return (
 			<Layout>
 				<div>
-					<link rel='stylesheet' href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css' />
 					<h3>Open Campaigns</h3>
 					<Button content='Create Campaign' icon='add' primary floated='right' />
 					{this.renderCampaigns()}
